4f4321ebec1cc484c8e4c4299806e9cc2424bd52
FeliQuintanilla <feli.duoc@duocuc.cl>
2024-09-10 03:07:24 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'recordatorioApp',
+  webDir: 'www'
+};
+
+export default config;
